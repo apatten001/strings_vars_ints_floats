11dc99c6df0ce451e178a228a06ea2f8eaa67f7f
Arnold <apatten001@yahoo.com>
2017-12-17 11:56:41 -0500

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,31 @@
+
+print('Exercise is a good thing that everyone can benefit from.')
+
+var1 = "This is turning a string into a variable"
+
+print(var1)
+
+# now i'm going to add two strings together
+
+print("I know im going to enjoy coding " + ","+ " especially once I've put the time in to learn.")
+
+# now lets print to add 2 blank lines in between strings
+
+print('\n' * 2)
+
+var2 = "This is the string after the blank lines"
+
+print(var2)
+
+#  here I am going to format with the strings
+
+print('\n')
+
+
+print("{} is a great {} to learn for my {} coding experience".format("Python","language", "first"))
+
+# now lets use % to add characters strings intergers and floats
+
+print("My favorite letter is %c. I love to %s.  I'm going to the bahamas in %i months. My college gpa was %.2f"
+      %('A','read', 3, 3.61))
+
